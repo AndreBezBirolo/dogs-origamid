636a34c7a20e336fe76aad1b61ac347b4f8450ad
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2023-11-24 11:22:13 -0300

    feat: criacao do primeiro hook, useForm

--- src/Components/Forms/Input.jsx
+++ src/Components/Forms/Input.jsx
@@ -1,13 +1,14 @@
 import styles from './Input.module.css'
 
 
-export const Input = ({label, type, name}) => {
+export const Input = ({label, type, name, value, onChange, error, onBlur}) => {
     return (
         <>
             <div className={styles.wrapper}>
                 <label htmlFor={name} className={styles.label}>{label}</label>
-                <input id={name} name={name} type={type} className={styles.input}></input>
-                <p className={styles.error}>Error</p>
+                <input id={name} name={name} type={type} className={styles.input} onChange={onChange}
+                       value={value} onBlur={onBlur}></input>
+                {error && <p className={styles.error}>{{error}}</p>}
             </div>
         </>
     )
--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -2,23 +2,23 @@ import {Link} from "react-router-dom";
 import React from "react";
 import Input from "../Forms/Input.jsx";
 import Button from "../Forms/Button.jsx";
+import useForm from "../../Hooks/UseForm.jsx";
 
 const LoginForm = () => {
-    const [username, setUsername] = React.useState('');
-    const [password, setPassword] = React.useState('');
+    const username = useForm();
+    const password = useForm();
 
     function handleSubmit(event) {
         event.preventDefault();
+
+        if (!(username.validate() && password.validate())) return;
 
         fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({
-                username,
-                password,
-            }),
+            body: JSON.stringify(),
         })
             .then((response) => {
                 console.log(response);
@@ -33,8 +33,8 @@ const LoginForm = () => {
     return <section>
         <h1>Login</h1>
         <form action='' onSubmit={handleSubmit}>
-            <Input name="username" label="User" type="text"></Input>
-            <Input name="password" label="Password" type="password"></Input>
+            <Input name="username" label="User" type="text" {...username}></Input>
+            <Input name="password" label="Password" type="password" {...password}></Input>
             <Button>Sign-in</Button>
             {/*<input type='text' value={username} onChange={({target}) => setUsername(target.value)}/>*/}
             {/*<input type='text' value={password} onChange={({target}) => setPassword(target.value)}/>*/}
